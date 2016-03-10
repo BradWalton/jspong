c9c73824aaaf8212e2655ceb9bde429e8cc66c34
BradWalton <walton4747@gmail.com>
2016-03-10 18:27:50 -0400

    setting up canvas

--- pong.js
+++ pong.js
@@ -5,3 +5,10 @@ var animate = window.requestAnimationFrame ||
     window.webkitCancelAnimationFrame ||
     window.mozRequestAnimationFrame ||
     function(callback) { window.setTimeout(callback, 1000/60) };
+
+var canvas = document.createElement('canvas');
+var width = 600;
+var hiegth = 800;
+canvas.width= width;
+canvas.hiegth= hiegth;
+var context= canvas.getContext('2d');
